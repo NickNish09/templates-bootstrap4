6c8435185272530d467316af9ba74b53a84dbf47
NickNish09 <nnmarques97@gmail.com>
2018-11-05 20:53:38 -0200

    att categorias para templates

--- open-all-templates.sh
+++ open-all-templates.sh
@@ -1,4 +1,28 @@
-for f in ./templates-folders/*
+for f in ./Templates-Admin/*
+do
+cd "${f}"
+google-chrome index.html
+cd ..
+cd ..
+done
+
+for f in ./Templates-Blog/*
+do
+cd "${f}"
+google-chrome index.html
+cd ..
+cd ..
+done
+
+for f in ./Templates-Lading-Page-Scroll/*
+do
+cd "${f}"
+google-chrome index.html
+cd ..
+cd ..
+done
+
+for f in ./Templates-Navegação/*
 do
 cd "${f}"
 google-chrome index.html
